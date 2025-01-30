c84a880a32bb5766a4f13f89bec1561f457603a2
Daichi Kudo <sakanactor624@gmail.com>
2025-01-30 20:42:01 +0900

    Remove RouterOutlet import from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FormComponent } from './component/form/form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormComponent],
+  imports: [FormComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
